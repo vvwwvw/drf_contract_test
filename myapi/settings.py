fb09b60ac569ac56f5f35c1930630ba80cf9d156
vvwwvw <minhoon1101@gamil.com>
2021-08-06 13:56:08 +0900

    final Ready

--- myapi/settings.py
+++ myapi/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware', #수정
+    
 ]
 
 ROOT_URLCONF = 'myapi.urls'
